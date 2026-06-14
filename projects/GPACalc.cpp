6baf26ae30b8cd95e87d46c5b63e571f24ed6883
haishhun <dmitryhaishun@gmail.com>
2026-06-14 01:56:32 -0400

    Clarified user prompt for continuing grade entry.

--- projects/GPACalc.cpp
+++ projects/GPACalc.cpp
@@ -109,7 +109,7 @@ int main()
         total_classes++;
 
         // Ask the user if he wants to enter another grade.
-        std::cout << "Do you want to continue? (Y/N) ";
+        std::cout << "Enter Y to add another grade. Any other key to exit.";
         std::cin >> continue_option;
         std::cin.ignore(10000, '\n'); // Handling inputs like "y3A+" etc.
 
