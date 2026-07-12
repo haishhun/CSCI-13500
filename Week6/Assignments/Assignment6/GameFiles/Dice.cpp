0eea89f08e88c2f98460862856a3a2b89d6c39d8
haishhun <dmitryhaishun@gmail.com>
2026-07-12 12:36:54 -0400

    Submit Assignment 6 - Finished

--- Week6/Assignments/Assignment6/GameFiles/Dice.cpp
+++ Week6/Assignments/Assignment6/GameFiles/Dice.cpp
@@ -1,13 +1,9 @@
 #include "Dice.h"
+
 #include <cstdlib>
 #include <ctime>
- 
-// TODO: Implement rollDice().
-// - Should return a random integer between 1 and 6, inclusive.
-// - Think carefully about where to seed the random number generator.
-//   If you seed it INSIDE this function, what happens every time it's called?
- 
+
 int RollDice() {
-    // TODO: replace this placeholder
-    return 0;
+  // srand() in main() of the game.
+  return rand() % 6 + 1;
 }
--- Week6/Assignments/Assignment6/GameFiles/Snake.cpp
+++ Week6/Assignments/Assignment6/GameFiles/Snake.cpp
@@ -1,56 +1,123 @@
 #include "Snake.h"
+
 #include <iostream>
 
-// TODO: Constructor
-// - Build a snake with STARTING_LENGTH segments using `new`.
-// - head should point to segment id 0 (or 1, your choice - just be consistent),
-//   tail should point to the last segment created.
-// - Set length and nextId appropriately.
 Snake::Snake() {
-    head_ = nullptr;
-    tail_ = nullptr;
-    length_ = 0;
-    id_ = 0;
-    // TODO: allocate STARTING_LENGTH nodes and link them together
+  head_ = nullptr;
+  tail_ = nullptr;
+  length_ = 0;
+  id_ = 0;
+
+  // Loop the amount of STARTING_LENGTH times.
+  for (int i = 0; i < STARTING_LENGTH; i++) {
+    // Create a new Node.
+    Node* newNode = new Node;
+    newNode->body_part_ = id_;
+    newNode->next_ = nullptr;
+    id_++;
+
+    // Validate if snake is empty then the first item becomes head and tail.
+    // Otherwise we add the node in the end.
+    if (head_ == nullptr) {
+      head_ = newNode;
+      tail_ = newNode;
+    } else {
+      tail_->next_ = newNode;
+      tail_ = newNode;
+    }
+
+    // Increment lenght_ in the end.
+    length_++;
+  }
 }
 
-// TODO: Destructor
-// - Walk the list and delete every remaining node.
-// - If you don't do this, running under valgrind/ASan will report a leak.
 Snake::~Snake() {
-    // TODO
+  // Declare a pointer current pointing to head_.
+  Node* current = head_;
+
+  // Loop through the list with deleting nodes.
+  while (current) {
+    // Declare a pointer next_node pointing to the current->next.
+    Node* next_node = current->next_;
+    delete current;
+    current = next_node;
+  }
 }
 
-// TODO: kill()
-// - Remove up to `count` nodes from the FRONT of the list, deleting each one.
-// - Handle the edge case where the snake has fewer than `count` nodes left:
-//   kill all remaining nodes and stop. Do not crash.
-// - Update `length` (and `tail`/`head` if the snake becomes empty!).
-// - Return the number of segments ACTUALLY killed.
 int Snake::Kill(int count) {
-    // TODO
-    return 0;
+  // Create a pointer current pointing to head_.
+  Node* current = head_;
+
+  // Declare variable for counting killed parts.
+  int killed_parts = 0;
+
+  // Loop through snake and kill parts from the front
+  for (int i = 0; i < count && current; i++) {
+    Node* next_node = current->next_;
+    delete current;
+    current = next_node;
+    killed_parts++;
+  }
+
+  // Reassign head_ to the first part that left after deleting.
+  head_ = current;
+
+  // Decrement length_ after deleting parts.
+  length_ = length_ - killed_parts;
+
+  // Validate if Snake is empty.
+  if (head_ == nullptr) {
+    tail_ = nullptr;
+  }
+
+  // Return the amount of deleted parts.
+  return killed_parts;
 }
 
-// TODO: regenerate()
-// - Append `count` new nodes to the BACK of the list using `new`.
-// - Use `nextId` to assign each new node a unique, increasing id, and
-//   increment `nextId` each time.
-// - Update `tail` (and `head` if the snake was empty!) and `length`.
 void Snake::Regenerate(int count) {
-    // TODO
+  // Loop count times and add new nodes.
+  for (int i = 0; i < count; i++) {
+    Node* newNode = new Node;
+    newNode->body_part_ = id_;
+    newNode->next_ = nullptr;
+    id_++;
+
+    // Validate if Snake is empty.
+    if (head_ == nullptr) {
+      head_ = newNode;
+      tail_ = newNode;
+    } else {
+      tail_->next_ = newNode;
+      tail_ = newNode;
+    }
+
+    length_++;
+  }
 }
 
-// TODO: print()
-// - Walk the list from head to tail and print in the format:
-//   Snake: [12] -> [13] -> [14] -> [15]
-// - If the snake is empty, print something sensible like "Snake: (dead)".
-// - This function must not modify the snake - use a `const Node*` to walk it.
 void Snake::PrintSnake() const {
-    // TODO
+  // Create a pointer current pointing to head_.
+  const Node* current = head_;
+
+  // Validate if Snake is empty.
+  if (head_ == nullptr) {
+    std::cout << "Snake: (dead)." << std::endl;
+    return;
+  }
+
+  // Print all snake parts.
+  std::cout << "Snake: ";
+  while (current) {
+    std::cout << "[" << current->body_part_ << "]";
+    if (current->next_) {
+      std::cout << " -> ";
+    }
+    current = current->next_;
+  }
+  std::cout << "\n";
 }
 
 int Snake::GetLength() const {
-    // TODO
-    return 0;
+  // Return the length of a snake
+  return length_;
 }
