a8e8a0c32dcf74a4801aba3bc086503c817d82b9
haishhun <dmitryhaishun@gmail.com>
2026-07-21 13:36:12 -0400

    Refactor AddThreeNumbers template to include three parameters

--- Week8/Templates/main.cpp
+++ Week8/Templates/main.cpp
@@ -1,17 +1,14 @@
 #include <iostream>
 
-template <typename T, typename U>
+template <typename T, typename U, typename V>
 T AddThreeNumbers(T a, U b, V c);
 
-int main(){
-
-    std::cout << AddThreeNumbers(6, 9, 7) << std::endl;
-    std::cout << AddThreeNumbers(6.7, 9.6, 0.0);
-
-
+int main() {
+  std::cout << AddThreeNumbers(6, 9, 7) << std::endl;
+  std::cout << AddThreeNumbers(6.7, 9.6, 0.0);
 }
 
-template <typename T, typename U>
-T AddThreeNumbers(T a, U b, V c){
-    return a + b + c;
+template <typename T, typename U, typename V>
+T AddThreeNumbers(T a, U b, V c) {
+  return a + b + c;
 }
